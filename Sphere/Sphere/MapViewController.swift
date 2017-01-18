a0a16cc35717dcd4e73e19c2bc14d6f511406037
Chuckry Vengadam <chuckry@umich.edu>
2017-01-18 15:59:07 -0500

    Remove MapView class for simplicity

--- Sphere/Sphere/MapViewController.swift
+++ Sphere/Sphere/MapViewController.swift
@@ -36,12 +36,22 @@ class MapViewController : UIViewController, CLLocationManagerDelegate {
     
     // Set up map to show current location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let loc : CLLocationCoordinate2D = manager.location!.coordinate
-        
-        // Set up the map
-        let mapView = MapView(location: loc, zoom: 12.0)
-        self.view = mapView.getMapView()
+        let loc = manager.location!.coordinate
+        self.view = renderGoogleMap(loc: loc, title: "NEEDS TITLE", snippet: "NEED SNIPPET")
         self.locationManager.stopUpdatingLocation()
+    }
+    
+    func renderGoogleMap(loc: CLLocationCoordinate2D, title: String?, snippet: String?) -> GMSMapView {
+        let camera = GMSCameraPosition.camera(withLatitude: loc.latitude, longitude: loc.longitude, zoom: 12.0)
+        let gmap = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        gmap.isMyLocationEnabled = true
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: loc.latitude, longitude: loc.longitude)
+        marker.title = title!
+        marker.snippet = snippet!
+        marker.map = gmap
+        return gmap
     }
     
     override func didReceiveMemoryWarning() {
