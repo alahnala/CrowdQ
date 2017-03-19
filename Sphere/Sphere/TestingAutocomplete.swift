867ef58ffbedcbd1e99d28f8282a14d205a8631c
alahnala <alahnala@github.com>
2017-03-19 17:33:27 -0400

    made test space

--- Sphere/Sphere/TestingAutocomplete.swift
+++ Sphere/Sphere/TestingAutocomplete.swift
@@ -7,3 +7,26 @@
 //
 
 
+import UIKit
+import Foundation
+
+class TestViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Feed"
+        displayText()
+    }
+    
+    func displayText() {
+        let textBox = UITextField(frame: CGRect(x: 100, y: 250, width: 200, height: 40.00))
+        self.view.addSubview(textBox)
+        textBox.backgroundColor = UIColor.red
+        textBox.text = "Welcome 2 tha f33d."
+        textBox.borderStyle = UITextBorderStyle.line
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
