32ea29b0ec683689f7ba0ad12de1142d307e18c3
Xinrui <xinruiyang1@gmail.com>
2017-03-21 15:14:30 -0400

    removed post request comment from UserTypeController

--- Sphere/Sphere/UserTypeController.swift
+++ Sphere/Sphere/UserTypeController.swift
@@ -116,38 +116,4 @@ class UserTypeController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /* --------- This might have to go in WhereViewController.swift ---------
-     
-     // Input: (spotifyUserId, name, venueName, lat, lng)
-     // Output: Success/Failure
-     func sendVendorInfo() {
-     let jsonPostString = ""
-     let getVenue = WhereViewController() // Get this from WhereViewController.swift
-     
-     // create post request to connect
-     let serverURL = "https://sgodbold.com:3000/registerVendor"
-     let url = URL(string: serverURL)!
-     var request = URLRequest(url: url)
-     request.httpMethod = "POST"
-     request.httpBody = jsonPostString.data(using: .utf8)
-     
-     let task = URLSession.shared.dataTask(with: request) { data, response, error in
-     guard let data = data, error == nil else {
-     print("error=\(error)")
-     return
-     }
-     
-     if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
-     print("response = \(response)")
-     }
-     
-     let responseString = String(data: data, encoding: .utf8)
-     print("responseString = \(responseString)")
-     }
-     task.resume()
-     }
-     
-    */
-    
 }
