a56f3a66a50f477facdbb1639a5b331e691fa70e
Xinrui <xinruiyang1@gmail.com>
2017-03-21 19:39:12 -0400

    updated venue renders in VenueView.swift and VenueViewController.swift

---
+++ Sphere/Sphere/VenueView.swift
@@ -0,0 +1,105 @@
+//
+//  VenueView.swift
+//  Sphere
+//
+//  Created by Chuckry Vengadam on 3/21/17.
+//  Copyright © 2017 EECS441. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+
+class VenueView : UIView {
+    var marker = GMSMarker() 
+    let backButton = UIButton(type: .system)
+    let checkinButton = UIButton()
+    let label = UILabel()
+    let checked = UILabel()
+    let theMoodsAreLabel = UILabel()
+    let genreLabel = UILabel()
+    
+    init(marker: GMSMarker)
+    {
+        self.marker = marker
+        super.init(frame: UIScreen.main.bounds)
+        
+        // back button
+        backButton.frame = CGRect(x: 8, y: 8, width: 50, height: 50)
+        backButton.setTitleColor(UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 1), for: .normal)
+        backButton.setTitleColor(UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.5), for: .selected)
+        backButton.setTitle("Back", for: .normal)
+        backButton.titleLabel!.font = UIFont.systemFont(ofSize: 20)
+        
+        // check in button
+        checkinButton.setTitle("Check-In", for: .normal)
+        checkinButton.backgroundColor = UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.75)
+        checkinButton.titleLabel!.font = UIFont.systemFont(ofSize: 26)
+        checkinButton.bounds = CGRect(x: 0, y: 0, width: 300, height: 100)
+        checkinButton.center = CGPoint(x: 187.5, y: 250)
+        checkinButton.layer.cornerRadius = 10
+        
+        
+        displayVenueName()
+        initializeAllViews()
+    }
+    
+    // venue name
+    func displayVenueName() {
+        //title
+        print("Displaying Venue Name")
+        label.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        //label.backgroundColor = UIColor.white
+        label.center = CGPoint(x: 187.5, y: 100)
+        label.textAlignment = .center
+        label.text = marker.title
+        label.textColor = UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.75)
+        label.font = label.font.withSize(32)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        print("\(label.text)")
+    }
+    
+    
+    func showGenre() {
+        
+        //call backend to get genre
+        let genres = ["Pop", "Rock", "Family Friendly"]
+        
+        // "The Moods for <Venue> are ... "
+        theMoodsAreLabel.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
+        theMoodsAreLabel.textColor = UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.75)
+        theMoodsAreLabel.center = CGPoint(x: 187.5, y: 200)
+        theMoodsAreLabel.textAlignment = .center
+        theMoodsAreLabel.text = "The moods for \(marker.title!) are listed as"
+        theMoodsAreLabel.font = label.font.withSize(20)
+        theMoodsAreLabel.lineBreakMode = .byWordWrapping
+        theMoodsAreLabel.numberOfLines = 0
+        
+        genreLabel.text = genres.joined(separator: ", ")
+        
+        genreLabel.frame = CGRect(x: 0, y: 200, width: 300, height: 100)
+        genreLabel.center = CGPoint(x: 187.5, y: 350)
+        genreLabel.font = label.font.withSize(32)
+        genreLabel.textAlignment = .center
+        genreLabel.textColor = UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.75)
+        genreLabel.lineBreakMode = .byWordWrapping
+        genreLabel.numberOfLines = 0
+    }
+    
+    func initializeAllViews() {
+        self.addSubview(backButton)
+        self.addSubview(checkinButton)
+        self.addSubview(label)
+        self.addSubview(checked)
+        self.addSubview(theMoodsAreLabel)
+        self.addSubview(genreLabel)
+        self.bringSubview(toFront: label)
+        print("zzz: initialize called")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        //super.init(coder: aDecoder)
+    }
+}
---
+++ Sphere/Sphere/VenueViewController.swift
@@ -0,0 +1,67 @@
+//
+//  VenueViewController.swift
+//  Sphere
+//
+//  Created by Allie Lahnala on 3/21/17.
+//  Copyright © 2017 EECS441. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import GoogleMaps
+
+class VenueViewController : UIViewController, CLLocationManagerDelegate, UITextFieldDelegate, GMSMapViewDelegate, GMSIndoorDisplayDelegate {
+    
+    var marker : GMSMarker
+    var venueView : VenueView
+    
+    init(marker: GMSMarker) {
+        self.marker = marker
+        self.venueView = VenueView(marker: self.marker)
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Venue"
+        self.view.addSubview(self.venueView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        print("zzz: made it here")
+        super.viewDidLoad()
+        print("zzz: view did load")
+        self.venueView.backButton.addTarget(self, action: #selector(self.backButtonPressed), for: .touchUpInside)
+        self.venueView.checkinButton.addTarget(self, action: #selector(self.checkinButtonPressed), for: .touchUpInside)
+    }
+    
+    func backButtonPressed() {
+        print("Back Button pressed!")
+        //change for better naming?
+        let mapTypeController = MapViewController()
+        self.present(mapTypeController, animated: true, completion: nil)
+    }
+    
+    // Called when venue button pressed
+    func checkinButtonPressed() {
+        // Send JSON info to server
+        print("Check-In Pressed")
+        
+        self.venueView.checkinButton.isHidden = true
+        //checked in
+        self.venueView.checked.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 100)
+        self.venueView.checked.textColor = UIColor(red: 169/255, green: 66/255, blue:103/255, alpha: 0.75)
+        self.venueView.checked.center = CGPoint(x: 187.5, y: 500)
+        self.venueView.checked.textAlignment = .center
+        self.venueView.checked.text = "Checked in!"
+        
+        self.venueView.checked.font = self.venueView.label.font.withSize(40)
+        
+        self.venueView.showGenre()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
