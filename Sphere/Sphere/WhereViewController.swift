61ecd32266a2f3ad796cd8608262e593bb81c62f
Xinrui <xinruiyang1@gmail.com>
2017-04-16 14:06:55 -0400

    took out commented post request function

--- Sphere/Sphere/WhereViewController.swift
+++ Sphere/Sphere/WhereViewController.swift
@@ -69,42 +69,7 @@ class WhereViewController : UIViewController, UITextFieldDelegate {
         let mapViewController = MapViewController()
         self.present(mapViewController, animated: true, completion: nil)
     }
-    
-    /* --------- Making POST request to the server ---------
-     
-     // Input: (spotifyUserId, name, venueName, lat, lng)
-     // Output: Success/Failure
-     
-     func sendVendorInfo() {
-        let jsonPostString = ""
-        let getVenue = WhereViewController() // Get this from WhereViewController.swift
-     
-        // create post request to connect
-        let serverURL = "https://sgodbold.com:3000/registerVendor"
-        let url = URL(string: serverURL)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = jsonPostString.data(using: .utf8)
-     
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print("error=\(error)")
-                return
-            }
-     
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
-            }
-     
-            let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(responseString)")
-        }
-            task.resume()
-     }
-     
-     */
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
